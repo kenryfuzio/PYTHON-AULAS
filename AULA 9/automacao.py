82eb9e7e6d8ed8a8124645981076486fbdd2cfba
Japozeno <94966801+kenryfuzio@users.noreply.github.com>
2022-11-17 21:58:35 -0300

    Update automacao.py

--- AULA 9/automacao.py
+++ AULA 9/automacao.py
@@ -5,4 +5,12 @@ system('cls')
 
 url = "https://economia.uol.com.br/"
 response = get(url)#aqui o requests lê a pagina e trás o html
-
+html_soup = BeautifulSoup(response.text, "html.parser")
+print(html_soup)
+#print(html_soup)
+#cotacao = html_soup.find_all('span', class_ = 'value bra')
+cotacao= html_soup.find_all('a', class_ ='subtituloGrafico subtituloGraficoValor')
+print(len(cotacao))
+print(cotacao)
+for valor in cotacao:
+    print(valor.text)
